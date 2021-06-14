b9e552d2d3dff1acf21db5fc9fc4400e4e8cff31
fernando <fernandomirandalolzeiro@gmail.com>
2021-06-14 18:38:10 -0300

    set initial settings for login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   namespace :api, default: { format: :json } do
+    mount_devise_token_auth_for 'User', at: 'auth'
     resources :teams
     resources :leagues
   end
