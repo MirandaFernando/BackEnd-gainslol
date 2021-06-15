f2ec42e9008e1da0d23a00448036996d739b5f95
fernando <fernandomirandalolzeiro@gmail.com>
2021-06-15 00:02:19 -0300

    Adding a line to enable email confirmation and adding settings to set the uid

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,20 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, 
+  devise :database_authenticatable,
+         :confirmable,
          :registerable,
          :recoverable,
          :rememberable,
          :validatable
+
   include DeviseTokenAuth::Concerns::User
+  before_validation :set_uid
+
+  private
+
+  def set_uid
+    self[:uid] = self[:email] if self[:uid].blank? && self[:email].present?
+  end
+
 end
