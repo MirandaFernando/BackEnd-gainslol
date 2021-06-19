334d7dface655287ec95abaf076db2c7ba93376b
fernando <fernandomirandalolzeiro@gmail.com>
2021-06-19 19:09:10 -0300

    fix conflits

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api, default: { format: :json } do
     mount_devise_token_auth_for 'User', at: 'auth'
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 <<<<<<< HEAD
 =======
@@ -11,6 +12,9 @@ Rails.application.routes.draw do
     resources :bets
 >>>>>>> added model and controller bet
 >>>>>>> added model and controller bet
+=======
+    resources :bets
+>>>>>>> fix conflits
     resources :teams
     resources :leagues
   end
