7e292c2404d20c5fb63e812280772081f2f4372e
fernando <fernandomirandalolzeiro@gmail.com>
2021-06-29 23:49:10 -0300

    configs deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,4 +110,18 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  # Email
+  config.action_mailer.default_url_options = {
+    host: 'course-blog-api.herokuapp.com'
+  }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    user_name: Rails.application.credentials.gmail[:user_name].to_s,
+    password: Rails.application.credentials.gmail[:password],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 end
