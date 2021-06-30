0be670ac46588e9e41bd32e59afa24048338f5db
fernando <fernandomirandalolzeiro@gmail.com>
2021-06-29 23:53:43 -0300

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,7 +112,7 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   # Email
   config.action_mailer.default_url_options = {
-    host: 'course-blog-api.herokuapp.com'
+    host: 'gains-lol-api.herokuapp.com'
   }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,10 +1,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins '*'
-  
-      resource '*',
-               headers: :any,
-               expose: %w[access-token expiry token-type uid client],
-               methods: %i[get post put delete options]
-    end
+  allow do
+    origins '*'
+
+    resource '*',
+              headers: :any,
+              expose: %w[access-token expiry token-type uid client],
+              methods: %i[get post put delete options]
   end
+end
